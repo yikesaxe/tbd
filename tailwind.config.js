34c3a18ec845537d69917307bdfc2d72a5ee6770
djuarez2652 <djuarez0440@gmail.com>
2024-10-05 19:40:50 -0400

    chore: add fluid plugin & add firebase config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import fluid, { extract } from 'fluid-tailwind'
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        extract
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        fluid
+    ],
 }
 
